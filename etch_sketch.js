54d7700498a1f0670599f05046dcc6bf0914cd36
foosh1 <allancarolan@gmail.com>
2022-01-24 21:11:20 -0500

    very basic working addition

--- etch_sketch.js
+++ etch_sketch.js
@@ -2,20 +2,33 @@ const grid = document.querySelector("#grid");
 
 let gridLength =prompt("Please enter the grid length");
 
+
 gridSize(gridLength);
 
 function gridSize(gridLength){
 
     let gridArea = (gridLength*gridLength);
 
-
     for(i=0;i<gridArea;i++){
         const newSqaure =document.createElement("div")
-        newSqaure.style.width =`${(512/gridLength)}px`;
+        
         newSqaure.classList.add("box");
+        newSqaure.addEventListener("mouseover",function (e){
+            console.log(e);
+            console.log(e.target);
+            e.target.classList.add("black");
+        });
+
+        newSqaure.style.width =`${(512/gridLength)}px`;
         grid.appendChild(newSqaure);
     }
     
+    const boxes=document.querySelectorAll(".box");
     
+    return;
+}
+
+function colorChange(){
+    newSquare.classList.add("black");
     return;
 }
